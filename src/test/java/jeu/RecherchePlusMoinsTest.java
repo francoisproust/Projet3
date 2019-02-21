407b49b97f324d358a60f6bf6d8d21e8e2849792
francois.proust@axa.fr <Th1nkCentre03%>
2019-02-21 09:52:02 +0100

    ajout class test

---
+++ src/test/java/jeu/RecherchePlusMoinsTest.java
@@ -0,0 +1,18 @@
+package jeu;
+
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class RecherchePlusMoinsTest {
+
+    @Test
+   public void comparaisonNombreAuDessus(){
+        RecherchePlusMoins jeu = new RecherchePlusMoins();
+
+
+    }
+}
